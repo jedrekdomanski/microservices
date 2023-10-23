5c33d7368537d1170fbe1e50faeae330259c25ca
Jędrzej Domański <jedrek.domanski@gmail.com>
2023-10-23 23:03:59 +0100

    Add episode 6 source code

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"log"
+	"fmt"
 	"net/http"
 	"strconv"
 	"github.com/gorilla/mux"
@@ -73,6 +74,18 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 		if err != nil {
 			p.l.Println("[ERROR] deserializing product", err)
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest,
+			)
 			return
 		}
 
